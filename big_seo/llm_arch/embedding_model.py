7e16e145db79c7fc8885ee4e32a3a8e4dbef7915
danhngan <danhnganc3@gmail.com>
2024-04-15 16:49:41 +0700

    delete previous llm

--- big_seo/llm_arch/embedding_model.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-from abc import ABC, abstractmethod
-
-
-class IEmbedding(ABC):
-    @abstractmethod
-    def get_embedding(self, text: str) -> list:
-        pass
